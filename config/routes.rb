985040bfb7a02f66e3bb67d4720f33a248241da1
John <john@Johns-MacBook-Pro.local>
2009-11-16 08:40:35 +0000

    moved some shoulda tests to RSpec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   
   map.resources :companies
+  map.resources :categories
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Company do
+  
   before(:each) do
     @company = Factory(:company)
   end
@@ -9,5 +10,14 @@ describe Company do
     @company_attributes = Factory.attributes_for(:company)
     Company.create!(@company_attributes)
   end
+    
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:address1) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:county) }
+  it { should validate_presence_of(:postcode) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:phone) }
   
 end
