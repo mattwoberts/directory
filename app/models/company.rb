b4d74c0757f93ec6ca7a3b0d8d30d174efef5746
John <john@Johns-MacBook-Pro.local>
2009-11-18 19:02:03 +0000

    oops... commented out validations to make debugging easier, forgot to put it back.

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,7 +5,7 @@ class Company < ActiveRecord::Base
   has_many :categories, :through => :categorizations
   
   # Validations
-  #validates_presence_of :name, :description, :address1, :city, :county, :postcode, :email, :phone
-  #validates_length_of   :name, :maximum => 30
+  validates_presence_of :name, :description, :address1, :city, :county, :postcode, :email, :phone
+  validates_length_of   :name, :maximum => 30
   
 end
